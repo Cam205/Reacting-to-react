74b80fb71df44e6a0735354ef85df8ad1c169752
Cam205 <cameronbuchanan17@yahoo.com>
2021-10-14 11:40:52 -0500

    Fixed some things with boot strap

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,31 @@ import Greeter from './Greeter'
 
 const App = () => {
     const [username, setUsername] = useState('')
+    const [hasLoaded, setLoaded] = useState(false)
 
-    return (
-        <>
-            <Greeter
-                phrase1="Hello there" name1="Cameron"
-                phrase2="Goodevening" name2="Kj"
-                phrase3="How are you doing" name3="Camille"
-            />
-            <input value = {username} onChange={e => setUsername(e.target.value)} />
-            <p>You are logging in as {username}</p>
-        </>
-    )
+    if (hasLoaded) {
+        return (
+            <>
+                <Greeter
+                    phrase1="Hello there" name1="Cameron"
+                    phrase2="Goodevening" name2="Kj"
+                    phrase3="How are you doing" name3="Camille"
+                />
+                <div class = "container">
+                <input value={username} onChange={e => setUsername(e.target.value)} />
+                <p>You are logging in as {username}</p>
+                </div>
+            </>
+        )
+    } else {
+        return (
+            <>
+                <h1>Website loading...</h1>
+ 
+                <button className = "btn btn-primary" onClick={() => setLoaded(true)}>Load Website</button>
+            </>
+        )
+    }
 }
 
 export default App;
--- src/Greeter.jsx
+++ src/Greeter.jsx
@@ -3,22 +3,26 @@ import React from "react";
 const Greeter = props => {
     return (
         <>
-            <div class="card">
-                <div class="card-body col-8">
-                    {props.phrase1} {props.name1}
-                </div>
-            </div>
 
-            <div class="card">
-                <div class="card-body col-8">
-                    {props.phrase2} {props.name2}
+            <div class="container">
+                <div class="card">
+                    <div class="card-body">
+                        {props.phrase1} {props.name1}
+                    </div>
                 </div>
-            </div>
 
-            <div class="card">
-                <div class="card-body col-8">
-                    {props.phrase3} {props.name3}
+                <div class="card">
+                    <div class="card-body">
+                        {props.phrase2} {props.name2}
+                    </div>
                 </div>
+
+                <div class="card">
+                    <div class="card-body">
+                        {props.phrase3} {props.name3}
+                    </div>
+                </div>
+
             </div>
         </>
     )
